91205c4153dde690f58e6aa50981b528f15843c9
Shyamarjun10 <Shyamarjuna102k4@gmail.com>
2024-05-02 20:47:29 +0530

    added userUpdate file BEnd

---
+++ models/userUpdate.js
@@ -0,0 +1,24 @@
+import axios from axios;
+
+class User {
+  static async createUser(formData) {
+    try {
+      console.log("data:", formData);
+      const response = await axios.post("http://localhost:3001/user/register", formData);
+      console.log("User registered successfully:", response.data);
+      return response.data; // Assuming the response contains the newly created user data
+    } catch (err) {
+      console.error("Error creating a new user:", err);
+      throw err;
+    }
+  }
+static async viewUsers() {
+    try {
+        const response = await axios.get("http://localhost:3001/user/viewUsers");
+        return response.data; 
+    } catch (err) {
+        console.error("Error fetching the users:", err);
+        throw err;
+    }
+}
+}
