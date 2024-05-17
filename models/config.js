66227eddd2126bb3b551decf89eea9a94f75af73
VISVESSWAR <amvisvesswar2004@gmail.com>
2024-05-17 10:58:42 +0530

    changes in prod.js prodcontroller.js prods.js config.js create.js

--- models/config.js
+++ models/config.js
@@ -1,13 +1,13 @@
-export default{
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "Hari@123",
-    DB: "users",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 5000,
-      idle: 3306,
-    },
+export default {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "Vishman2@sql",
+  DB: "users",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 5000,
+    idle: 3306,
+  },
 };
